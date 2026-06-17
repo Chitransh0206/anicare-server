bf1513c1630c3ee92122d771242a0c9bd2f0d669
Chitransh Jain <chitransh0206jain@gmail.com>
2026-06-17 09:35:37 +0530

    updated vet schema with new fields

---
+++ prisma/migrations/20260617040044_vet_update/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE `Vet` ADD COLUMN `address` VARCHAR(191) NULL,
+    ADD COLUMN `experience` INTEGER NULL,
+    ADD COLUMN `image` VARCHAR(191) NULL,
+    ADD COLUMN `specialization` VARCHAR(191) NULL,
+    ADD COLUMN `timing` VARCHAR(191) NULL;
